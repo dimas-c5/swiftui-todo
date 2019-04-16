aa5f73fd69d7759e61f7c6616c2ff8f24007393d
Prakash Venkat <venkatraman.prakash@gmail.com>
2019-04-15 17:09:52 -0700

    first commit

--- Foo/ViewController.swift
+++ Foo/ViewController.swift
@@ -1,20 +1,21 @@
-//
-//  ViewController.swift
-//  Foo
-//
-//  Created by Prakash Venkatraman on 3/25/19.
-//  Copyright © 2019 Prakash Venkatraman. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-
+final class ViewController: UIViewController {
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var mainLabel: UILabel!
 }
 
+// MARK: - View Life Cycle
+extension ViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
+
+// MARK: - Actions
+private extension ViewController {
+    @IBAction func actionButton(_ sender: UIButton) {
+        mainLabel.text = textField.text
+    }
+}
