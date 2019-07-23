f2aacb78ecf1f489159ee61d575738dc3b460852
Tim Kersey <t@k-t.im>
2019-07-22 17:47:53 -0700

    Add UserDefault property wrapper

---
+++ Todo/UserDefault.swift
@@ -0,0 +1,56 @@
+import Foundation
+
+@propertyWrapper
+struct UserDefault<Value: PropertyListValue> {
+    let key: String
+    let defaultValue: Value
+    var userDefaults: UserDefaults
+
+    init(_ key: String, defaultValue: Value, userDefaults: UserDefaults = .standard) {
+        self.key = key
+        self.defaultValue = defaultValue
+        self.userDefaults = userDefaults
+    }
+
+    var wrappedValue: Value {
+        get {
+            return userDefaults.object(forKey: key) as? Value ?? defaultValue
+        }
+        set {
+            userDefaults.set(newValue, forKey: key)
+        }
+    }
+}
+
+protocol PropertyListValue {}
+
+extension Data: PropertyListValue {}
+extension NSData: PropertyListValue {}
+
+extension String: PropertyListValue {}
+extension NSString: PropertyListValue {}
+
+extension Date: PropertyListValue {}
+extension NSDate: PropertyListValue {}
+
+extension NSNumber: PropertyListValue {}
+extension Bool: PropertyListValue {}
+extension Int: PropertyListValue {}
+extension Int8: PropertyListValue {}
+extension Int16: PropertyListValue {}
+extension Int32: PropertyListValue {}
+extension Int64: PropertyListValue {}
+extension UInt: PropertyListValue {}
+extension UInt8: PropertyListValue {}
+extension UInt16: PropertyListValue {}
+extension UInt32: PropertyListValue {}
+extension UInt64: PropertyListValue {}
+extension Double: PropertyListValue {}
+extension Float: PropertyListValue {}
+#if os(macOS)
+extension Float80: PropertyListValue {}
+#endif
+
+extension Array: PropertyListValue where Element: PropertyListValue {}
+
+extension Dictionary: PropertyListValue where Key == String, Value: PropertyListValue {}
