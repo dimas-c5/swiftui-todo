c6c23f4d1c8732f40b30195084f45987abaccdf9
Tim Kersey <t@k-t.im>
2020-01-07 16:35:58 -0800

    Include addReducer

---
+++ Add/AddReducer.swift
@@ -0,0 +1,6 @@
+public func addReducer(state: inout String, action: AddAction) {
+    switch action {
+    default:
+        break
+    }
+}
--- Todo/AppReducer.swift
+++ Todo/AppReducer.swift
@@ -1,6 +1,8 @@
 import ComposableArchitecture
+import Add
 import Show
 
 let appReducer: (inout AppState, AppAction) -> Void = combine(
+    pullback(addReducer, value: \.textInput, action: \.add),
     pullback(showReducer, value: \.todos, action: \.show)
 )
